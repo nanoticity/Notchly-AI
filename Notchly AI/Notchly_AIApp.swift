f71d850af221ca868af0aa7964a0e430ee4877d4
Nano <nanoticity@gmail.com>
2025-07-21 11:28:26 +0200

    Initial Commit

---
+++ Notchly AI/Notchly_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notchly_AIApp.swift
+//  Notchly AI
+//
+//  Created by Ari Nemet on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Notchly_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Notchly AITests/Notchly_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  Notchly_AITests.swift
+//  Notchly AITests
+//
+//  Created by Ari Nemet on 7/21/25.
+//
+
+import Testing
+@testable import Notchly_AI
+
+struct Notchly_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
